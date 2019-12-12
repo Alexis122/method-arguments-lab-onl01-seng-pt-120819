69e39fd33616857ea73970e88906e85afec0e80c
Alexis Priore <alexispriore@gmail.com>
2019-12-12 01:53:04 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,3 +9,7 @@ end
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
+
+def greeting_programmer(name, language)
+  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+end
